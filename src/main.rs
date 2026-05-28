5dc23aa9473d77acc07062d9a2e53f2ab101a966
kev1n999 <sopratesta999@gmail.com>
2026-05-28 13:52:32 -0300

    define a const variable to storage the server host

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ use crate::server::connection;
 
 use std::net::TcpListener;
 
+const HOST: &str = "127.0.0.1:8000";
+
 fn main() {
-    let stream = TcpListener::bind("127.0.0.1:8000").expect("An error ocurred to bind!");
+    let stream = TcpListener::bind(HOST).expect("An error ocurred to bind socket!");
     for stream in stream.incoming() {
         let stream = stream.unwrap();
         connection::handle_connection(stream);
